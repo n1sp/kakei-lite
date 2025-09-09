89979a92d700dc7174018062c15c0dddf78357b6
n1sp <n1sp.pc001@gmail.com>
2025-09-10 08:42:59 +0900

    feat:implementation of a simple main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <></>
+    <main>
+      <h1>Kakei Lite</h1>
+      <div className="flex gap-4">
+        <Link href="/expense/dashboard">支出一覧</Link>
+        <Link href="/expense/additional">支出追加</Link>
+      </div>
+    </main>
   )
 }
