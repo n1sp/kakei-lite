c1ccdb8ad5367341e9a756c4508c96c152061e28
n1sp <n1sp.pc001@gmail.com>
2025-09-12 08:08:52 +0900

    chore:Remove useless comments

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,6 @@ export default function Home() {
 
       {/* ページ遷移リンク */}
       <div className="flex gap-4">
-        {/* <Link href="/expense/dashboard" className="px-4 py-2 rounded-xl bg-blue-500 hover:bg-blue-600 transition-colors duration-150">支出一覧</Link>
-        <Link href="/expense/additional" className="px-4 py-2 rounded-xl bg-blue-500 hover:bg-blue-600 transition-colors duration-150">支出追加</Link> */}
         <Button asChild>
           <Link href="/expense/dashboard">支出一覧</Link>
         </Button>
